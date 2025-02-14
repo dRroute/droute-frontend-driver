5dc5213fe31f47a5f21e8f539e347db13eac7ed9
Alok Singh <thealoksinghh@gmail.com>
2025-02-15 00:47:05 +0530

    map layout created

--- lib/screens/map/Map_Page.dart
+++ lib/screens/map/Map_Page.dart
@@ -1,16 +1,54 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
-  const MapPage({super.key});
-
   @override
-  State<MapPage> createState() => _MapPageState();
+  _MapPageState createState() => _MapPageState();
 }
 
 class _MapPageState extends State<MapPage> {
+  late BitmapDescriptor customIcon;
+
+  final LatLng vanLocation = LatLng(18.465932, 73.834730);
+  final LatLng destLocation = LatLng(18.469118, 73.834063);
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCustomMarker();
+  }
+
+  void _loadCustomMarker() async {
+    customIcon = await BitmapDescriptor.fromAssetImage(
+      const ImageConfiguration(size: Size(20, 20)), // Adjust size
+      'assets/images/MapMarker.png',
+    );
+    setState(() {}); // Refresh UI
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(target: vanLocation, zoom: 13),
+        markers:
+            customIcon == null
+                ? {}
+                : {
+                  Marker(
+                    markerId: MarkerId("van_marker"),
+                    position: vanLocation,
+                    icon: customIcon,
+                  ),
+                  Marker(
+                    markerId: MarkerId("destination_marker"),
+                    position: destLocation,
+                    icon:
+                        BitmapDescriptor
+                            .defaultMarker, // Default marker for destination
+                  ),
+                },
+      ),
+    );
   }
 }
