b670285b173b2a583bd633502e6ec339ab96054f
cyboravi <cyboravidell@gmail.com>
2025-06-03 17:48:13 +0530

    Backend api constant imported in document api file

--- src/utils/api/documentApi.js
+++ src/utils/api/documentApi.js
@@ -2,12 +2,10 @@ import { apiPostRequest } from "../http/post";
 import { APP_BACKEND_API } from "@env";
 import { apiGetRequest } from "../http/get";
 import { apiPutRequest } from "../http/put";
-import { DRIVER_API_URL } from "./authApi";
+import Key from "../../constants/key"; // Import Key object
 
-
-// const USER_API_URL = 'https://ba36-2409-40d0-3034-b1bb-9c5f-b08d-66b5-ae62.ngrok-free.app'+"/api/user";
-
-// const accessToken = async ()=> await AsyncStorage.getItem("accessToken");
+// Constants
+const { DRIVER_API_URL } = Key;
 // API CALLS
 export const uploadSingleDocumentAPI = (data) =>
   apiPostRequest({
